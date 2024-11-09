a8a1ca5130c134b4fab0881469493f51a09b56cf
youchun <149042855+tanyouchun@users.noreply.github.com>
2024-11-09 21:57:38 +0800

    Update manageJob.php

--- Job Seeker/manageJob.php
+++ Job Seeker/manageJob.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FlexMatch</title>
-    <link rel="stylesheet" href="manageJob.css">
+    <link rel="stylesheet" href="css/manageJob.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="manageJob.js" defer></script>   
+    <script src="js/manageJob.js" defer></script>   
 </head>
 <body>
 
