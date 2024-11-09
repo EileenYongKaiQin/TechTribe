3070cbabc5a7c6114f72b9a8ed34042dc0f7eb7e
Oh Kai Xuan <116958204+OKX03@users.noreply.github.com>
2024-11-09 21:22:31 +0800

    Create submit_post.php

---
+++ Job Seeker/submit_post.php
@@ -0,0 +1,32 @@
+<?php
+    include('../database/config.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = "Ali";
+    $skillCategory = $_POST['skills'];
+    $skillDetails = $_POST['skillDetails'];
+    $availability = json_encode([
+        'monday' => [$_POST['mondayStart'], $_POST['mondayEnd']],
+        'tuesday' => [$_POST['tuesdayStart'], $_POST['tuesdayEnd']],
+        'wednesday' => [$_POST['wednesdayStart'], $_POST['wednesdayEnd']],
+        'thursday' => [$_POST['thursdayStart'], $_POST['thursdayEnd']],
+        'friday' => [$_POST['fridayStart'], $_POST['fridayEnd']],
+        'saturday' => [$_POST['saturdayStart'], $_POST['saturdayEnd']],
+        'sunday' => [$_POST['sundayStart'], $_POST['sundayEnd']]
+    ]);
+    $state = $_POST['state'];
+    $district = $_POST['district'];
+    $jobPreferences = $_POST['jobPreferences'];
+    $contactEmail = $_POST['contactEmail'];
+    $contactPhone = $_POST['contactPhone'];
+
+    $stmt = $conn->prepare("INSERT INTO wall_posts (user_name, skill_category, skill_details, availability, state, district, job_preferences, contact_email, contact_phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssssss", $username, $skillCategory, $skillDetails, $availability, $state, $district, $jobPreferences, $contactEmail, $contactPhone);    
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+
+    header("Location: job_seeker_wall.php");
+    exit();
+}
+
