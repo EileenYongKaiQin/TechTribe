782fdb7d44dc82057056ccdba14a2c5e4955442f
tamjiahao0417 <tamhao@graduate.utm.my>
2024-11-11 14:39:14 +0800

    Update chat.php

--- database/chat.php
+++ database/chat.php
@@ -6,7 +6,7 @@ $password = "";
 $dbname = "flexMatch_db";  // Replace with your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+//hi
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
