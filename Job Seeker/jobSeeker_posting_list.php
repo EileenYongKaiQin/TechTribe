9c7f52fb3da20ed18a76fc0fe7b6fac690741b6d
EILEEN YONG KAI QIN <eileenkai@graduate.utm.my>
2024-12-19 01:02:32 +0800

    Update jobSeeker_posting_list.php

--- Job Seeker/jobSeeker_posting_list.php
+++ Job Seeker/jobSeeker_posting_list.php
@@ -68,6 +68,42 @@
         .search-bar-container {
                     top: 105px;
         }
+
+        .date, .salary, .location, .time {
+                margin: 0;
+            }
+
+            .date, .salary, .location {
+                padding-left: 50px;      
+            }
+
+            .date {
+                margin-bottom: -18px;    
+                font-weight: bold;   
+            }
+
+            .salary { 
+                margin-top: -5px;
+                margin-bottom: 25px;   
+                font-weight: bold;   
+                color: #333333;
+            }
+
+            .location {
+                display: flex;
+                align-items: center; 
+                margin-bottom: 5px;  
+                color: #767F8C;
+            }
+
+            .time {
+                color: #767F8C;      
+            }
+
+            .salary-line {
+                border-top: 1px solid #dcdcdc; /* Light grey line */
+                margin: 0px 0; /* Adjust vertical spacing */
+            }
     </style>
 </head>
 <body>
@@ -96,32 +132,39 @@
                 foreach ($jobs as $job) {
             ?>
 
-        <div class="job-card" data-id="<?php echo $job['jobPostID']; ?>">
-            <div class="top-right-icons">
-            <img src="../images/BookmarkSimple.png" alt="Bookmark" class="bookmark-icon" onclick="saveJob('<?php echo $job['jobPostID']; ?>', this)">
-                <img src="../images/more_vert.png" alt="More Options" class="more-options-icon" onclick="toggleDropdown(this)">
-                <div class="dropdown-menu">
-                    <div class="dropdown-item" onclick="navigateToReport('<?php echo $job['jobPostID']; ?>')">
-                        <img src="../images/sms_failed.png" alt="Report Icon">
-                        <span>Report Post</span>
-                    </div>
-                    <div class="dropdown-item" onclick="hidePost(this)">
-                        <img src="../images/cancel_presentation.png" alt="Hide Icon">
-                        <span>Hide Post</span>
+            <div class="job-card" data-id="<?php echo $job['jobPostID']; ?>">
+                <div class="top-right-icons">
+                    <img src="../images/BookmarkSimple.png" alt="Bookmark" class="bookmark-icon" onclick="saveJob('<?php echo $job['jobPostID']; ?>', this)">
+                    <img src="../images/more_vert.png" alt="More Options" class="more-options-icon" onclick="toggleDropdown(this)">
+                    <div class="dropdown-menu">
+                        <div class="dropdown-item" onclick="navigateToReport('<?php echo $job['jobPostID']; ?>')">
+                            <img src="../images/sms_failed.png" alt="Report Icon">
+                            <span>Report Post</span>
+                        </div>
+                        <div class="dropdown-item" onclick="hidePost(this)">
+                            <img src="../images/cancel_presentation.png" alt="Hide Icon">
+                            <span>Hide Post</span>
+                        </div>
                     </div>
                 </div>
-            </div>
 
                 <div class="job-header">
                     <h3><?php echo htmlspecialchars($job['jobTitle']); ?></h3>
                 </div>
                 <div class="job-details">
-                    <p class="salary">
+                    <!-- Working Hours -->
+                    <p class="time">
                         <span class="time-label <?php echo ($job['workingHour'] === 'Day Shift') ? 'day' : 'night'; ?>">
                             <?php echo htmlspecialchars($job['workingHour']); ?>
                         </span>
-                        Salary: RM <?php echo number_format($job['salary'], 2); ?> / hour
+                        <?php 
+                            $workingTimeStart = date("h:i A", strtotime($job['workingTimeStart']));
+                            $workingTimeEnd = date("h:i A", strtotime($job['workingTimeEnd']));
+                            echo $workingTimeStart . " - " . $workingTimeEnd;
+                        ?>
                     </p>
+
+                    <!-- Date -->
                     <p class="date">
                         <?php 
                             $startDate = date("d/m/Y", strtotime($job['startDate']));
@@ -129,13 +172,21 @@
                             echo $startDate . " - " . $endDate;
                         ?>
                     </p>
+
+                    <!-- Venue -->
                     <p class="location">
-                        <img src="../images/MapPin.png" alt="Location Icon" class="map-pin"> <?php echo htmlspecialchars($job['location']); ?>
+                        <img src="../images/MapPin.png" alt="Location Icon" class="map-pin"> 
+                        <?php echo htmlspecialchars($job['venue']); ?>, <?php echo htmlspecialchars($job['location']); ?>
+                    </p>
+
+                    <div class="salary-line"></div>
+                    <!-- Salary -->
+                    <p class="salary">
+                        Salary: RM <?php echo number_format($job['salary'], 2); ?> / hour
                     </p>
 
                     <div class="job-details-buttons">
                         <button class="view-details-btn" onclick="location.href='manageJob.php?jobPostID=<?php echo $job['jobPostID']; ?>'">View Details</button>
-
                     </div>
                 </div>
             </div>
