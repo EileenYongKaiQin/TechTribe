d7e1f573fae78561f8e953050a0a1deb8ee5d9a0
Jessie Chang <147798314+jessiechang03@users.noreply.github.com>
2024-12-19 04:42:45 +0000

    password strength login

--- sql/login.sql
+++ sql/login.sql
@@ -10,24 +10,24 @@ CREATE TABLE login (
 
 -- Insert an admin into login
 INSERT INTO login (userID, username, password, role)
-VALUES ('AD001', 'kaixuan', MD5('kaixuan@123'), 'admin');
+VALUES ('AD001', 'kaixuan', MD5('Kaixuan@123'), 'admin');
 
 -- Insert an employer into login
 INSERT INTO login (userID, username, password, role)
-VALUES ('EP001', 'eileen', MD5('eileen@123'), 'employer');
+VALUES ('EP001', 'eileen', MD5('Eileen@123'), 'employer');
 INSERT INTO login (userID, username, password, role)
-VALUES ('EP002', 'emjessie', MD5('jc123'), 'employer');
+VALUES ('EP002', 'emjessie', MD5('Jessie@123'), 'employer');
 INSERT INTO login (userID, username, password, role)
-VALUES ('EP003', 'alice', MD5('alice123'), 'employer');
+VALUES ('EP003', 'alicee', MD5('Alice@123'), 'employer');
 INSERT INTO login (userID, username, password, role)
-VALUES ('EP004', 'bob', MD5('bob123'), 'employer');
+VALUES ('EP004', 'nabilah', MD5('Nabilah@123'), 'employer');
 
 -- Insert a jobseeker into login
 INSERT INTO login (userID, username, password, role)
-VALUES ('JS001', 'qiqi03', MD5('chuaqi@123'), 'jobSeeker');
+VALUES ('JS001', 'qiqi03', MD5('Chuaqi@123'), 'jobSeeker');
 INSERT INTO login (userID, username, password, role)
-VALUES ('JS002', 'jsjessie', MD5('jc123'), 'jobSeeker');
+VALUES ('JS002', 'jsjessie', MD5('Jessie@123'), 'jobSeeker');
 INSERT INTO login (userID, username, password, role)
-VALUES ('JS003', 'bernice', MD5('bernice123'), 'jobSeeker');
+VALUES ('JS003', 'bernice', MD5('Bernice@123'), 'jobSeeker');
 INSERT INTO login (userID, username, password, role)
-VALUES ('JS004', 'jesslyn', MD5('kek123'), 'jobSeeker');
+VALUES ('JS004', 'jesslyn', MD5('Jesslyn@123'), 'jobSeeker');
