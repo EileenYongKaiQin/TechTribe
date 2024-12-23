dbd5165d1dc6170509cc3dbfa80d48a93fabe9a3
Jessie Chang <147798314+jessiechang03@users.noreply.github.com>
2024-12-23 00:43:58 +0000

    css update

--- Job Seeker/view_selfprofile_jobseeker.php
+++ Job Seeker/view_selfprofile_jobseeker.php
@@ -35,7 +35,7 @@ if (!$data) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Profile</title>
 
-    <link rel="stylesheet" href="../css/view_selfprofile copy.css">
+    <link rel="stylesheet" href="../css/view_selfprofile.css">
 
 </head>
 
